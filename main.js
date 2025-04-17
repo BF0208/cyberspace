df802170e5c95dbef38d7e6be3c88b5c653f6b61
BF0208 <110055961+BF0208@users.noreply.github.com>
2025-04-17 00:09:44 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 import * as THREE from 'https://unpkg.com/three@0.154.0/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.154.0/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.154.0/examples/jsm/controls/OrbitControls.js';
 import { gsap } from 'https://unpkg.com/gsap@3.12.5/index.js';
 
 let scene, camera, renderer, controls;
